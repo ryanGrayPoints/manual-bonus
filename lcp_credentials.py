5f344882709fd45e8a8e9d4940e3694bfbff241e
ryanGrayPoints <30698596+ryanGrayPoints@users.noreply.github.com>
2020-12-11 15:29:09 -0500

    Create lcp_credentials.py

---
+++ lcp_credentials.py
@@ -0,0 +1,11 @@
+# STAGING
+user_mac_key_id = ''
+user_mac_key = ''
+app_mac_key_id = ''
+app_mac_key = ''
+
+# PRODUCTION
+#user_mac_key_id = ''
+#user_mac_key = ''
+#app_mac_key_id = ''
+#app_mac_key = ''
